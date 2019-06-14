f5c7ec3eeec96b7fae311507c43de59dba31264b
JiaYue <jialedang@163.com>
2019-06-14 17:32:33 +0800

    添加订单功能

---
+++ src/com/jiayue/OrderServlet.java
@@ -0,0 +1,20 @@
+package com.jiayue;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created with IntelliJ IDEA
+ * Created By JiaYue
+ * Date: 2019/6/14
+ * Time: 17:30
+ */
+public class OrderServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+}
